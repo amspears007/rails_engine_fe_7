e11eb7388b57f7aa61df64c24a6ec185d3bca0c7
Amy Spears <amspears007@gmail.com>
2023-07-19 16:37:45 -0600

    Add gems

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -1,12 +1,13 @@
 require "rails_helper"
 
+
 describe "Merchants Index API Page" do
-   it "I visit 'api/v1/merchants and should see a list of merchant names with a link to their show page " do
+   it "I visit 'api/v1/merchants and should see a list of merchant names with a link to their show page" do
     visit "/api/v1/merchants"
 save_and_open_page
-    expect(page).to have_content("Schroeder-Jerde")
-    expect(page).to have_content("Klein, Rempel and Jones")
-    expect(page).to have_content( "Wisozk, Hoeger and Bosco")
+    expect(page).to have_link("Schroeder-Jerde")
+    expect(page).to have_link("Klein, Rempel and Jones")
+    expect(page).to have_link( "Wisozk, Hoeger and Bosco")
   end
 end
 
