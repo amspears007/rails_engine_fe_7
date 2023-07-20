da480a83b110c4c15dc15639070b498b0aa314fc
Amy Spears <amspears007@gmail.com>
2023-07-19 20:00:14 -0600

    Call items in view page to render name list.  Finish US1

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -3,16 +3,14 @@ class MerchantsController < ApplicationController
     conn = Faraday.new(url: "http://localhost:3000")
     response = conn.get("/api/v1/merchants")
     json_response = JSON.parse(response.body, symbolize_names: true)
-    # require 'pry'; binding.pry
     @merchants = json_response[:data]
-    # require 'pry'; binding.pry
   end
 
   def show
     merchant_response = Faraday.get("http://localhost:3000/api/v1/merchants/#{params[:id]}")
     merch_json = JSON.parse(merchant_response.body, symbolize_names: true)
     @merchant = merch_json[:data]
-    # require 'pry'; binding.pry
+   
 
     item_response = Faraday.get("http://localhost:3000/api/v1/merchants/#{params[:id]}/items")
     item_json = JSON.parse(item_response.body, symbolize_names: true)
