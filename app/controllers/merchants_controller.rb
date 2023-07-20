bf97a3f9ceb6d1efa6602a0e88b8dc351d7067da
Amy Spears <amspears007@gmail.com>
2023-07-19 19:58:09 -0600

    Add get requests for single merchant and items assocaited with it

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -9,6 +9,13 @@ class MerchantsController < ApplicationController
   end
 
   def show
-    
+    merchant_response = Faraday.get("http://localhost:3000/api/v1/merchants/#{params[:id]}")
+    merch_json = JSON.parse(merchant_response.body, symbolize_names: true)
+    @merchant = merch_json[:data]
+    # require 'pry'; binding.pry
+
+    item_response = Faraday.get("http://localhost:3000/api/v1/merchants/#{params[:id]}/items")
+    item_json = JSON.parse(item_response.body, symbolize_names: true)
+    @items = item_json[:data]
   end
 end
